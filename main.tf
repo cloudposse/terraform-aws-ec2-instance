1ea1aa724ff71228b3af62e57d78c6c087043d8e
SweetOps <vladimir.fess@gmail.com>
2017-09-26 14:16:28 +0300

    remove unsued atributes

--- main.tf
+++ main.tf
@@ -19,12 +19,11 @@ data "aws_iam_policy_document" "default" {
 
 # Apply the tf_label module for this resource
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  delimiter  = "${var.delimiter}"
-  attributes = ["${var.instance_enabled ? var.instance_enabled : ""}"]
+  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "${var.name}"
+  delimiter = "${var.delimiter}"
 }
 
 resource "aws_iam_instance_profile" "default" {
