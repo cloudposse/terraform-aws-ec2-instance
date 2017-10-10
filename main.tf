c6f057fd7030cda113262c5880a43e2c4625d904
Sergey Vasilyev <s2504s@gmail.com>
2017-10-10 19:09:26 +0300

    Added locals

--- main.tf
+++ main.tf
@@ -29,7 +29,8 @@ module "label" {
 }
 
 locals {
-  instance_count = "${var.instance_enabled ? 1 : 0}"
+  instance_count        = "${var.instance_enabled ? 1 : 0}"
+  create_security_group = "${var.create_default_security_group ? 1 : 0}"
 }
 
 resource "aws_iam_instance_profile" "default" {
@@ -47,7 +48,7 @@ resource "aws_iam_role" "default" {
 }
 
 resource "aws_security_group" "default" {
-  count       = "${var.create_default_security_group}"
+  count       = "${local.create_security_group}"
   name        = "${module.label.id}"
   vpc_id      = "${var.vpc_id}"
   description = "Instance default security group (only egress access is allowed)"
