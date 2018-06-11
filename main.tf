60f3510baee0ec2d7da3e0cf76c2aa8b4dfadc04
Ivan Pinatti <ivan-pinatti@users.noreply.github.com>
2018-06-11 15:21:53 -0400

    Update ubuntu version (#30)
    * Fix tags_as_list_of_maps error
    
    * Re-add tags as variables
    
    * Bump Ubuntu version to Bionic 18.04

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ data "aws_ami" "default" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
   filter {
