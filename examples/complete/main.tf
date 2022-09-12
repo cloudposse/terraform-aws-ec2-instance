b95c7b9b6b407ea1992cfa4ed5450ebad31efdf9
Tommy <thomas.poetke@exconcept.com>
2022-09-12 22:04:43 +0200

    fix examples to be inline with new module versions

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -17,7 +17,7 @@ module "vpc" {
   source  = "cloudposse/vpc/aws"
   version = "1.1.1"
 
-  cidr_block = "172.16.0.0/16"
+  ipv4_primary_cidr_block = "172.16.0.0/16"
 
   context = module.this.context
 }
@@ -28,8 +28,8 @@ module "subnets" {
 
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
+  igw_id               = ["${module.vpc.igw_id}"]
+  ipv4_cidr_block      = ["${module.vpc.vpc_cidr_block}"]
   nat_gateway_enabled  = false
   nat_instance_enabled = false
 
