9f67ab2ee65043eacd66e249b12cdb25dd878470
Tommy <thomas.poetke@exconcept.com>
2022-09-12 23:24:46 +0200

    remove deprecated syntax

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -28,8 +28,8 @@ module "subnets" {
 
   availability_zones   = var.availability_zones
   vpc_id               = module.vpc.vpc_id
-  igw_id               = ["${module.vpc.igw_id}"]
-  ipv4_cidr_block      = ["${module.vpc.vpc_cidr_block}"]
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
   nat_gateway_enabled  = false
   nat_instance_enabled = false
 
