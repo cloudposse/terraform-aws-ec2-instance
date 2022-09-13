64fff8a2b23cc521c7e58e4508d61db799221531
Tommy <thomas.poetke@exconcept.com>
2022-09-13 23:14:05 +0200

    fix provider in examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = var.region
+}
 
 module "aws_key_pair" {
   source              = "cloudposse/key-pair/aws"
--- examples/complete/provider.tf
+++
@@ -1,7 +0,0 @@
-
-provider "aws" {
-  region = "us-east-2"
-  assume_role {
-    role_arn = "arn:aws:iam::128840427886:role/OrganizationAccountAccessRole"
-  }
-}
