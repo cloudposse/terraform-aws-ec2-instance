e1432be3cf08c36a6776e169e1644f9b73f58459
Andy Roth <roth.andy@gmail.com>
2022-10-20 14:40:24 -0700

    Add input validation

--- variables.tf
+++ variables.tf
@@ -373,4 +373,8 @@ variable "tenancy" {
   type        = string
   default     = "default"
   description = "Tenancy of the instance (if the instance is running in a VPC). An instance with a tenancy of 'dedicated' runs on single-tenant hardware. The 'host' tenancy is not supported for the import-instance command. Valid values are 'default', 'dedicated', and 'host'."
+  validation {
+    condition     = contains(["default", "dedicated", "host"], lower(var.tenancy))
+    error_message = "Tenancy field can only be one of default, dedicated, host"
+  }
 }
